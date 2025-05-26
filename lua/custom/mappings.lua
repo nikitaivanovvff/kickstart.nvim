9d305019bf205a47f48855569b35ccc7902401e7
nikitaivanovvff <nikitaivanovv.ff@gmail.com>
2025-05-26 19:37:50 +0400

    tabs mappings

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -2,9 +2,11 @@
 vim.keymap.set('n', '<leader>j', function()
   require('gitsigns').nav_hunk 'next'
 end, { desc = 'Next Git hunk' })
-
 vim.keymap.set('n', '<leader>k', function()
   require('gitsigns').nav_hunk 'prev'
 end, { desc = 'Previous Git hunk' })
-
 vim.keymap.set('n', '<leader>ga', ':Git add %<CR>', { desc = '[G]it [a]dd current file' })
+
+-- tabs
+vim.keymap.set('n', '<leader>tn', ':tabnew<CR>', { desc = 'New tab' })
+vim.keymap.set('n', '<leader>tc', ':tabclose<CR>', { desc = 'Close tab' })
