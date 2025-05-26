2833c9672929820efd3a5eb546a7ee384b180875
nikitaivanovvff <nikitaivanovv.ff@gmail.com>
2025-05-26 19:27:37 +0400

    change few mappings

--- init.lua
+++ init.lua
@@ -434,11 +434,13 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>ff', builtin.live_grep, { desc = '[F]ind in [F]iles' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+      vim.keymap.set('n', '<leader>rf', builtin.oldfiles, { desc = 'Search [R]ecent [F]iles' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      -- stolen from IntellijIdea
+      vim.keymap.set('n', '<leader>sp', builtin.lsp_document_symbols, { desc = '[S]tructure [P]opup' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -6,3 +6,5 @@ end, { desc = 'Next Git hunk' })
 vim.keymap.set('n', '<leader>k', function()
   require('gitsigns').nav_hunk 'prev'
 end, { desc = 'Previous Git hunk' })
+
+vim.keymap.set('n', '<leader>ga', ':Git add %<CR>', { desc = '[G]it [a]dd current file' })
