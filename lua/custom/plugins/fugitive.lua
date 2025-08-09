944690e05ca4daf64d9e4a2be6e7be6e6e655a5a
nikita.ivanov <nikitaivanovv.ff@gmail.com>
2025-08-09 12:33:50 +0400

    change fugitive mappings

--- lua/custom/plugins/fugitive.lua
+++ lua/custom/plugins/fugitive.lua
@@ -1,10 +1,13 @@
 return {
-  {
-    'tpope/vim-fugitive',
-    cmd = { 'Git', 'G', 'Gdiffsplit', 'Gvdiffsplit', 'Gedit', 'Gwrite', 'Gread', 'Ggrep' },
-    keys = {
-      { '<leader>gs', ':Git<CR>', desc = 'Git status (fugitive)' },
-      { '<leader>gb', ':Git blame<CR>', desc = 'Git blame' },
-    },
-  },
+  'tpope/vim-fugitive',
+
+  config = function()
+    vim.keymap.set('n', '<leader>gs', ':Git<CR>', { noremap = true, silent = true, desc = 'Git status' })
+    vim.keymap.set('n', '<leader>gb', ':Git blame<CR>', { noremap = true, silent = true, desc = 'Git blame' })
+    vim.keymap.set('n', '<leader>gd', ':Gdiffsplit<CR>', { noremap = true, silent = true, desc = 'Git diff' })
+    vim.keymap.set('n', '<leader>ga', ':Gwrite<CR>', { noremap = true, silent = true, desc = 'Git Add current file' })
+    vim.keymap.set('n', '<leader>go', ':Git checkout ', { noremap = true, silent = false, desc = 'Git Checkout ' })
+    vim.keymap.set('n', '<leader>gn', ':Git checkout -b ', { noremap = true, silent = false, desc = '[G]it Checkout [N]ew Branch' })
+    vim.keymap.set('n', '<leader>gc', ':Git commit<CR>', { noremap = true, silent = false, desc = '[G]it [C]ommit' })
+  end,
 }
